f7960f7391155158b194711c1142d1fbbda2ae32
silviasuciuu <62290505+silviasuciu@users.noreply.github.com>
2020-12-23 19:38:17 +0200

    lab3

--- src/todo/Student.tsx
+++ src/todo/Student.tsx
@@ -1,74 +1,25 @@
 import React from 'react';
-import {Redirect} from "react-router-dom";
-import {useHistory} from "react-router-dom";
-import {
-    IonItem,
-    IonLabel,
-    IonButton,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonLoading,
-    IonList, IonFab, IonFabButton, IonIcon, IonPage,
-} from '@ionic/react';
+import {IonItem, IonLabel, IonButton,} from '@ionic/react';
 import {StudentProps} from './StudentProps';
-import {add} from "ionicons/icons";
-import {RouteComponentProps} from "react-router";
 
 interface StudentPropsExt extends StudentProps {
     onEdit: (id?: string) => void;
 }
-let history = useHistory();
 
-// @ts-ignore
 const Student: React.FC<StudentPropsExt> = ({_id, nume, prenume, grupa, active, onEdit}) => {
     return (
 
 
-        <IonPage>
-            <IonHeader>
 
-            </IonHeader>
-            <IonContent>
-                <IonItem onClick={() => onEdit(_id)}>
+        <IonItem onClick={() => onEdit(_id)}>
 
-                    <IonLabel>{_id}</IonLabel>
-                    <IonLabel>{nume}</IonLabel>
-                    <IonLabel>{prenume}</IonLabel>
-                    <IonLabel>{grupa}</IonLabel>
-                    <IonLabel>{active}</IonLabel>
+            <IonLabel>{_id}</IonLabel>
+            <IonLabel>{nume}</IonLabel>
+            <IonLabel>{prenume}</IonLabel>
+            <IonLabel>{grupa}</IonLabel>
+            <IonLabel>{active}</IonLabel>
 
-                </IonItem>
-                <IonButton onClick={() => history.push('/login')
-                }>Logout</IonButton>
-            </IonContent>
-        </IonPage>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-             <IonItem onClick={() => onEdit(_id)}>
-
-                 <IonLabel>{_id}</IonLabel>
-                 <IonLabel>{nume}</IonLabel>
-                 <IonLabel>{prenume}</IonLabel>
-                 <IonLabel>{grupa}</IonLabel>
-                 <IonLabel>{active}</IonLabel>
-
-             </IonItem>
-             */
+        </IonItem>
     );
 
 };
